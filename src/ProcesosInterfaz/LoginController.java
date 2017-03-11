6b9ed1eb91c63af38368fbabb1b807b72fb48226
miguelangelchgz <miguelangelchgz@gmail.com>
2017-03-11 01:06:41 -0400

    Eliminadas clases innecesarias y se movio la clase controladora del login

--- src/ProcesosInterfaz/LoginController.java
+++ src/ProcesosInterfaz/LoginController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interfaz;
+package ProcesosInterfaz;
 
 import java.net.URL;
 import java.util.ResourceBundle;
