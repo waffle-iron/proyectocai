e446857068e63a6ea32daa0687d7bb81bf90251e
miguelangelchgz <miguelangelchgz@gmail.com>
2017-03-12 21:44:35 -0400

    Comentario sobre la scene

--- src/Interfaz/Interfaz.java
+++ src/Interfaz/Interfaz.java
@@ -24,7 +24,7 @@ public class Interfaz extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
-        
+        //Esto pone la ventana transparte y sin opciones
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
